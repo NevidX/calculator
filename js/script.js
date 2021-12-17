681f3a9bb35b3b62a558101de2226488737f66fe
NevidX <433android@mail.ru>
2021-12-17 17:49:31 +0300

    Added fucntions: + - / * equals
    BUGS:
    
    - button <== watch in windows calculator
    - do something with default value ZERO or Nothing(place holder now) in current and previous values.
    - might be issues with operatorIsChoosed and equalsIsPressed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 let calc = document.querySelector('.calc')
 let currentNum = document.forms.formCurent.textview
 let previousNum = document.forms.formPrevious.textview
+// ============Operations===============
+let div = '÷'
+let multi = '×'
+let sub = '-'
+let add = '+'
+
+// ======================================
+
+// STATES ==========================
+let equalsIsPressed = false
+let operatorIsChoosed = false
+// ================================
+
 // ===============================================================
 // <<<>>>
 CalcLoop()
 // <<<>>>
-
 getNum()
-// clearCurrentArea()
+// clearValueArea()
 // clearSymb()
 
 // ===============================================================
@@ -20,13 +32,25 @@ function CalcLoop() {
 		let currentAction = event.target.getAttribute('data-value');
 		switch (currentAction) {
 			case 'clearAll':
-				clearCurrentArea()
+				clearValueArea()
 				break;
 			case 'clearSymb':
 				clearSymb()
 				break;
 			case 'div':
-				previousArea('/')
+				previousArea(div)
+				break;
+			case 'multi':
+				previousArea(multi)
+				break;
+			case 'sub':
+				previousArea(sub)
+				break;
+			case 'add':
+				previousArea(add)
+				break;
+			case 'equals':
+				getResult()
 				break;
 		}
 	})
@@ -39,25 +63,77 @@ function CalcLoop() {
 // ===============FUNCTIONS==========================================
 function getNum() {
 	calc.addEventListener('click', (event) => {
-		if (event.target.classList.contains('num')) {
-			let curentNumValue = event.target.getAttribute('data-value');
-			currentNum.value =
-				currentNum.value + curentNumValue;
+		let targetContainsNum = event.target.classList.contains('num')
+		if (targetContainsNum && equalsIsPressed == true) {
+			clearValueArea()
+			currentNum.value += event.target.getAttribute('data-value')
+		}
+		else if (targetContainsNum && operatorIsChoosed == false) {
+			currentNum.value += event.target.getAttribute('data-value')
+		}
+		else if (targetContainsNum && operatorIsChoosed == true) {
+			currentNum.value = '';
+			currentNum.value += event.target.getAttribute('data-value')
+			operatorIsChoosed = false
 		}
 	})
 }
 
-function clearCurrentArea() {
-	currentNum.value = '';
+function getResult() {
+	equalsIsPressed = true
+	let previousNumFloat = parseFloat(previousNum.value.match(/\d/g).join(''))
+	let currentNumFloat = parseFloat(currentNum.value)
+	let currentOperation = previousNum.value.replace(/[0-9]/g, '').replace(/\s+/g, '')
+	switch (currentOperation) {
+		case div:
+			previousArea(div)
+			currentNum.value = String(previousNumFloat / currentNumFloat);
+			break;
+		case multi:
+			previousArea(multi)
+			currentNum.value = String(previousNumFloat * currentNumFloat);
+			break;
+		case sub:
+			previousArea(sub)
+			currentNum.value = String(previousNumFloat - currentNumFloat);
+			break;
+		case add:
+			previousArea(add)
+			currentNum.value = String(previousNumFloat + currentNumFloat);
+			break;
+	}
+}
+
+function clearValueArea() {
+	clearCurrentArea()
+	clearPreviousArea()
+	equalsIsPressed = false
+	operatorIsChoosed = false
+}
+
+function clearPreviousArea() {
 	previousNum.value = '';
 }
 
+function clearCurrentArea() {
+	currentNum.value = '';
+}
 function previousArea(operator) {
-	previousNum.value = currentNum.value + ' ' + operator
+	if (equalsIsPressed == false) {
+		operatorIsChoosed = true
+		previousNum.value = currentNum.value + ' ' + operator
+	} else {
+		previousNum.value = previousNum.value + ' ' + currentNum.value + ' ' + '='
+	}
 }
 
 function clearSymb() {
-	currentNum.value = currentNum.value.slice(0, -1)
+	if (operatorIsChoosed == true && equalsIsPressed == false) {
+		currentNum.value = currentNum.value.slice(0, -1)
+	} else {
+		clearPreviousArea()
+	}
+
 }
 
 
